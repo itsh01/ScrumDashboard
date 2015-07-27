7ded30037ff9197b55aa8b89c9af483eab990cd0
Itay Shtekel <shtekel1@gmail.com>
2015-07-27 17:13:21 +0300

    required lodash

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by itaysh on 7/27/15.
  */
+
+requirejs.config({
+    baseUrl: "../src",
+    paths: {
+        lodash: "lib/lodash/lodash"
+    },
+    shim: {
+        lodash: {
+            exports: "_"
+        }
+    }
+});
+
+requirejs(["lodash"],function (_) {
+    console.log(_);
+});
