0e548fe4940ace4d854a1437276e5f51cc7e5028
Itay Shtekel <shtekel1@gmail.com>
2015-07-28 10:04:54 +0300

    fixed paths for process html

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
             },
             build: {
                 src: 'src/vendor/require.js',
-                dest: 'build/js/require.js'
+                dest: 'build/vendor/require.js'
             }
         },
         react: {
