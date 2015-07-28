8da8e9552795d9b5966cf6ef0ac1f187d071e168
Tom Enden <tome@wix.com>
2015-07-28 12:31:04 +0300

    removed comment: jsx React.DOM

--- src/js/components/MainContainer.jsx
+++ src/js/components/MainContainer.jsx
@@ -6,8 +6,6 @@ define(['lodash', 'React'], function (_, React) {
     'use strict';
 
 
-    /** jsx React.DOM */
-
     return React.createClass({
         displayName: 'Main',
         render: function () {
