20e5cd70db5126986839fe0b920a2aee23fb8d79
Tom Enden <tome@wix.com>
2015-07-28 11:17:02 +0300

    removed mainContainer.js, and added displayName

--- src/js/components/MainContainer.jsx
+++ src/js/components/MainContainer.jsx
@@ -7,6 +7,7 @@ define(['lodash', 'React'], function (_, React) {
     /** jsx React.DOM */
 
     return React.createClass({
+        displayName: 'Main',
         render: function () {
             return <div>Scrum Board</div>
         }
