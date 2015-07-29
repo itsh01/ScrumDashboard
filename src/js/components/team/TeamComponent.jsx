92cd7d88f7d967293b21a068db617defa30b8b64
Maya Schuster <mayasc@wix.com>
2015-07-29 15:18:28 +0300

    added style and fixed bugs

--- src/js/components/team/TeamComponent.jsx
+++ src/js/components/team/TeamComponent.jsx
@@ -14,40 +14,59 @@ define(['lodash', 'React', 'components/team/ChangeSprint', 'components/sprint/Ta
             },
 
             getInitialState: function () {
-                var team = this.context.flux.teamsStore.getTeamById(this.props.currTeamId);
-                return {
-                    team: team,
-                    currSprint: team.sprints[team.sprints.length - 1],
-                    currSprintIndex: team.sprints.length - 1
-                };
+                return this.getSprintValues(this.props);
+            },
+
+            componentWillReceiveProps: function (nextProps) {
+                this.setState(this.getSprintValues(nextProps));
             },
 
             handleSprintChange: function (direction) {
                 var allSprints = this.context.flux.teamsStore.getTeamById(this.props.currTeamId).sprints;
                 if (direction === 'backwards' && this.state.currSprintIndex !== 0) {
-                    this.setState({currSprint: allSprints[this.state.currSprintIndex - 1], currSprintIndex: this.state.currSprintIndex - 1});
+                    this.setState({
+                        currSprint: allSprints[this.state.currSprintIndex - 1],
+                        currSprintIndex: this.state.currSprintIndex - 1
+                    });
                 } else if (direction === 'forward' && this.state.currSprintIndex !== this.state.team.sprints.length - 1) {
-                    this.setState({currSprint: allSprints[this.state.currSprintIndex + 1], currSprintIndex: this.state.currSprintIndex + 1});
+                    this.setState({
+                        currSprint: allSprints[this.state.currSprintIndex + 1],
+                        currSprintIndex: this.state.currSprintIndex + 1
+                    });
                 }
             },
 
+            getTeamObject: function (teamId) {
+                return this.context.flux.teamsStore.getTeamById(teamId);
+            },
+
+            getSprintValues: function (props) {
+                var team = this.getTeamObject(props.currTeamId);
+                return {
+                    currSprint: team.sprints[team.sprints.length - 1],
+                    currSprintIndex: team.sprints.length - 1
+                };
+            },
+
             render: function () {
+                var team = this.context.flux.teamsStore.getTeamById(this.props.currTeamId);
                 return (<div>
-                    <h1>{this.state.team.name} Team</h1>
+                    <h1>{team.name} Team</h1>
 
                     <h2>Scrum DashBoard</h2>
 
                     <div className="flex-centered one-row">
                         <ChangeSprint direction='backwards'
                                       handleSprintChangeFunc={this.handleSprintChange.bind(this, 'backwards')}/>
+
                         <h3>Sprint: {this.state.currSprintIndex}</h3>
                         <ChangeSprint direction='forward'
                                       handleSprintChangeFunc={this.handleSprintChange.bind(this, 'forward')}/>
                     </div>
 
-                    <div className="flex-centered  one-row">
+                    <div className="flex-base  one-row">
                         <div className="backlog">
-                            <BackLog teamId={this.state.team.id}/>
+                            <BackLog teamId={team.id}/>
                         </div>
                         <SprintTable sprint={this.state.currSprint}/>
                     </div>
