ed2e7f25855503e32db7a483841fcb1e809fd473
tomr <tomr@wix.com>
2015-07-30 20:39:06 +0300

    added addCard functionality, still buggy on edge cases

---
+++ src/js/helpers/helpers.js
@@ -0,0 +1,16 @@
+define([], function () {
+    'use strict';
+    return {
+        generateGuid: function () {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        }
+    };
+});
+
--- src/js/stores/cards.js
+++ src/js/stores/cards.js
@@ -1,5 +1,4 @@
-
-define(['lodash', '../data/cards'], function (_, defaultCardsData) {
+define(['lodash', '../data/cards', '../helpers/helpers'], function (_, defaultCardsData, helpers) {
     'use strict';
     var filterFunctions = {
         AllCards: null,
@@ -24,16 +23,38 @@ define(['lodash', '../data/cards'], function (_, defaultCardsData) {
             return _.cloneDeep(_.find(currentCards, {id: id}));
         };
 
-        this.getBlankCard = function () {
-            return {
-                name: '',
-                description: '',
-                score: null,
-                team: null,
-                assignee: null,
-                startDate: null
-            };
+        var cardSchema = {
+            name: {type: 'string', defaultValue: 'New Card'},
+            description: {type: 'string', defaultValue: ''},
+            score: {type: 'number', defaultValue: null},
+            team: {type: 'string', defaultValue: null},
+            status: {type: 'string', defaultValue: 'unassigned'},
+            assignee: {type: 'string', defaultValue: null},
+            startDate: {type: 'string', defaultValue: null},
+            endDate: {type: 'string', defaultValue: null}
         };
+
+        this.getBlankCard = function () {
+            return _.assign({}, cardSchema, function (objectValue, sourceValue) {
+                return sourceValue.defaultValue === undefined ? '' : sourceValue.defaultValue;
+            });
+        };
+
+        function isValidValue(value, key) {
+            // make sure that all required fields are provided
+            if (!value && cardSchema[key].defaultValue !== undefined) {
+                console.log('Card Store: key(', key, ') was required but not provided');
+                return false;
+            } else if (typeof value !== cardSchema[key].type && value !== cardSchema[key].defaultValue) {
+                console.log('Card Store: invalid value encountered: ', value, ' (key: ', key, ')');
+                return false;
+            }
+            return true;
+        }
+
+        function isValidCard(card) {
+            return _.every(card, isValidValue);
+        }
 
         /**
          *
@@ -47,10 +68,38 @@ define(['lodash', '../data/cards'], function (_, defaultCardsData) {
          *  )
          */
         function addCard(newCardData) {
-            currentCards.push(newCardData);
+            var blankCard = this.getBlankCard(),
+                cardWithDefaults = _.assign(blankCard, newCardData);
+            if (isValidCard(cardWithDefaults)) {
+                cardWithDefaults.id = helpers.generateGuid();
+                cardWithDefaults.status = cardWithDefaults.status || 'unassigned';
+                currentCards.push(cardWithDefaults);
+                return cardWithDefaults.id;
+            }
         }
 
-        dispatcher.registerAction('ADD_MEMBER', addCard.bind(this));
+        /**
+         *
+         * @param {Object} newCardData (
+         *  name: [String],
+         *  description: [String] (optional, default: ''),
+         *  score: [Number] (optional, default: null),
+         *  team: [String] (optional, default: null),
+         *  assignee: [String] (optional, default: null),
+         *  startDate: [String] (optional, format: YYYY-MM-DD, default: null)
+         *  )
+         */
+        //function updateCard(cardId, newCardData) {
+        //    if (isValidCard(newCardData)) {
+        //        newCardData = JSON.parse(JSON.stringify(newCardData));
+        //        newCardData.id = dispatcher.generateGuid();
+        //        currentCards.push(newCardData);
+        //        return newCardData.id;
+        //    }
+        //}
+
+        //dispatcher.registerAction('UPDATE_MEMBER', updateCard.bind(this));
+        dispatcher.registerAction('ADD_CARD', addCard.bind(this));
     }
 
     return CardsStore;
