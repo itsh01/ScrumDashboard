4b412740ed7ec5894b09d88dd503e380308a7264
Tom Enden <tome@wix.com>
2015-07-30 17:33:54 +0300

    added flux to TeamView

--- src/js/components/TeamManagement/TeamView.jsx
+++ src/js/components/TeamManagement/TeamView.jsx
@@ -5,13 +5,24 @@ define(['lodash', 'React'], function (_, React) {
         propTypes: {
             team: React.PropTypes.object
         },
+        contextTypes: {
+            flux: React.PropTypes.any
+        },
 
         getTeamTitle: function () {
             return 'Team ' + this.props.team.name;
         },
         render: function () {
             return (
-                <h1>{this.getTeamTitle()}</h1>
+                <div>
+                    <h1>{this.getTeamTitle()}</h1>
+                    {
+                        _.map(this.props.team.members, function (memberId) {
+                            var member = this.context.flux.membersStore.getMemberById(memberId);
+                            return <MemberProfile member={member} key={memberId}/>;
+                        }, this)
+                    }
+                </div>
             );
         }
     });
