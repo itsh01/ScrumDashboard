0efa0179c4b213d784d740315f658189335d59f2
Maya Schuster <mayasc@wix.com>
2015-08-02 17:55:43 +0300

    fix build

--- src/js/components/pop-up/Basic.jsx
+++ src/js/components/pop-up/Basic.jsx
@@ -1,5 +1,12 @@
 define(['React'], function (React) {
+    'use strict';
     return React.createClass({
+        displayName: 'BasicPopUp',
+
+        propTypes: {
+            children: React.PropTypes.element
+        },
+
         render: function () {
             return (
                 <div className='pop pop-basic'>
