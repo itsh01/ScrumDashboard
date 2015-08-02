47a2aacaa2edd8eb016836e53675b51a2c8808c3
Tzabar Cohen <tzabar.cohen@gmail.com>
2015-08-02 12:13:44 +0300

    adding company list

--- src/js/components/backlog/Backlog.jsx
+++ src/js/components/backlog/Backlog.jsx
@@ -17,10 +17,16 @@ define([
             contextTypes: {
                 flux: React.PropTypes.any
             },
+            getInitialState: function () {
+                return {
+
+                };
+            },
             render: function () {
                 var teamCards = this.context.flux.cardsStore.getTeamCards(this.props.teamId);
                 return (<div>
                     <CardsList title="Team backlog" cardsList={teamCards}/>
+                    <CardsList title="Company backlog" cardsList={teamCards}/>
                 </div>);
             }
         });
