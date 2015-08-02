1723129d6385ddf90b858753251f0783b0e239a7
Itay Shtekel <shtekel1@gmail.com>
2015-08-02 18:11:33 +0300

    added drop type validation

--- src/js/mixins/DragDropMixin.js
+++ src/js/mixins/DragDropMixin.js
@@ -67,7 +67,9 @@ define([],
                 e.preventDefault();
             },
             handleDrop: function (e) {
-                var jsonData = e.dataTransfer.getData('data');
+                var jsonData = e.dataTransfer.getData('objToPass'),
+                    passedObj = JSON.parse(jsonData),
+                    acceptableDrops = this.dragDropData.acceptableDrops;
 
                 e.preventDefault();
 
@@ -75,12 +77,18 @@ define([],
                     throw new Error('Must define drop function when using droppable');
                 }
 
-                this.dragDropData.drop(JSON.parse(jsonData));
+                if (_.includes(acceptableDrops, passedObj.dropType)) {
+                    this.dragDropData.drop(passedObj.data);
+                }
+
             },
             handleDragStart: function (e) {
-                var data = this.dragDropData.dataTransfer;
+                var objToPass = {
+                        data: this.dragDropData.dataTransfer,
+                        dropType: this.dragDropData.dropType
+                    };
 
-                e.dataTransfer.setData('data', JSON.stringify(data));
+                e.dataTransfer.setData('objToPass', JSON.stringify(objToPass));
             }
         };
     }
