dc9e4823e9694bb91166029a64dddaa998dbab02
Itay Shtekel <shtekel1@gmail.com>
2015-08-05 13:43:35 +0300

    addded map to cards function to HistoryMixin

--- src/js/mixins/HistoryMixin.js
+++ src/js/mixins/HistoryMixin.js
@@ -1,3 +1,19 @@
 /**
  * Created by itaysh on 8/5/15.
  */
+
+define(['lodash'],
+    function (_) {
+        'use strict';
+
+        return {
+            mapHistoryToCards: function (history) {
+                var card = this.context.flux.cardsStore.getCardById(history.cardId);
+                card.assignee = history.assigneeId;
+                card.status = history.status;
+
+                return card;
+            }
+        };
+    }
+);
