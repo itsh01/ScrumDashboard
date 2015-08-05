3f88514f23dc819376f10acb8205696f9639fcf6
Maria Osipova <mariao@wix.com>
2015-08-05 15:21:35 +0300

    cards container: heap view v0

--- src/js/components/sprint/CardsContainer.jsx
+++ src/js/components/sprint/CardsContainer.jsx
@@ -8,46 +8,67 @@ define([
 
         return React.createClass({
             displayName: 'Sprint Cards Container',
+
             propTypes: {
                 cards: React.PropTypes.array
             },
 
             getInitialState: function () {
                 return {
-                    openCardId: null
+                    openCardId: null,
+                    cards: this.props.cards
                 };
             },
 
-            cardClicked: function (cardId) {
+            stackCardClicked: function (cardId) {
+                var openCardId = (this.state.openCardId === cardId) ? null : cardId;
+                var cards = this.state.cards;
+                if (cards[cards.length - 1].id !== cardId) {
+                    var topCard = _.remove(cards, {id: cardId})[0];
+                    cards.push(topCard);
+                }
+                this.setState({openCardId: openCardId, cards: cards});
+            },
+
+            heapCardClicked: function (cardId) {
                 var openCardId = (this.state.openCardId === cardId) ? null : cardId;
                 this.setState({openCardId: openCardId});
             },
 
-            getStackCardStyle: function (card, cardIndex) {
+            getStackCardStyle: function (cardIndex, card) {
                 var cardStyle = {
-                    top: cardIndex * 0.6 + 'rem',
-                    left: cardIndex * 0.6 + 'rem'
+                    transform: 'translate(' + (cardIndex * 0.6) + 'rem, ' + (cardIndex * 0.6) + 'rem)'
                 };
-                cardStyle.zIndex = (this.state.openCardId === card.id) ? this.props.cards.length : cardIndex;
+                cardStyle.zIndex = (this.state.openCardId === card.id) ? this.state.cards.length : cardIndex;
                 return cardStyle;
             },
 
             getHeapCardStyle: function (cardIndex) {
-                return {
-                    top: 0,
-                    left: 0,
-                    zIndex: cardIndex
-                };
+                if (this.state.openCardId === null) {
+                    return { };
+                }
+                var cardsNum = this.state.cards.length;
+                var t = 2 * Math.PI * cardIndex / cardsNum;
+                var r = 10;
+                var a = 0, b = 0; // center
+                var x = a + r * Math.cos(t), y = b + r * Math.sin(t);
+                return {transform: 'translate(' + x + 'rem, ' + y + 'rem)'};
+            },
+
+            getHandlers: function () {
+                if (this.state.cards.length > 3) {
+                    return {style: this.getHeapCardStyle, click: this.heapCardClicked};
+                }
+                return {style: this.getStackCardStyle, click: this.stackCardClicked};
             },
 
             render: function () {
-                var cardsNum = this.props.cards.length,
-                    cards = _.map(this.props.cards, function (card, cardIndex) {
-                        var cardStyle = (cardsNum < 5) ? this.getStackCardStyle(card, cardIndex) : this.getHeapCardStyle(card);
-                        return (<div style={cardStyle} key={card.id} className="sprint-card-wrapper">
-                            <Card card={card} cardClickHandler={this.cardClicked}/>
+                var handlers = this.getHandlers(),
+                    cards = _.map(this.state.cards, function (card, cardIndex) {
+                        return (<div style={handlers.style(cardIndex, card)} key={card.id} className="sprint-card-wrapper">
+                            <Card card={card} cardClickHandler={handlers.click} />
                         </div>);
-                    }.bind(this));
+                    });
 
                 return (<div className="sprint-cards-container">
                     {cards}
