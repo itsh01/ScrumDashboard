4bddea2aa845be75cff9b88bb5f22847b0dd4e76
Maya Schuster <mayasc@wix.com>
2015-08-05 15:20:49 +0300

    more changes in Edit Sprint

--- src/js/components/team/EditSprint.jsx
+++ src/js/components/team/EditSprint.jsx
@@ -5,11 +5,6 @@ define(['lodash', 'React', 'constants'],
         return React.createClass({
             displayName: 'EditSprint',
 
-            propTypes: {
-                currTeamId: React.PropTypes.string,
-                sprintId: React.PropTypes.substringData
-            },
-
             contextTypes: {
                 flux: React.PropTypes.any
             },
@@ -17,9 +12,28 @@ define(['lodash', 'React', 'constants'],
             mixins: [React.addons.LinkedStateMixin],
 
             getInitialState: function () {
-                var sprint = this.context.flux.teamsStore.getSprintById(this.props.sprintId);
-                console.log(sprint);
-                return sprint;
+                return this.context.flux.teamsStore.getCurrentSprint();
+            },
+
+            teamMemberOptionsBox: function (callback) {
+                var teamMembers = this.context.flux.teamsStore.getCurrentTeam().members;
+                var options = _.map(teamMembers, function (member) {
+                    var memberName = this.context.flux.membersStore.getMemberById(member).name;
+                    return (<option value={member} key={member}>{memberName}</option>);
+                }.bind(this));
+                return (<select onChange={callback}>
+                    <option selected disabled>-</option>
+                    {options}
+                </select>);
+            },
+
+            teamMembersCheckBoxes: function () {
+                var teamMembers = this.context.flux.teamsStore.getCurrentTeam().members;
+                var options = _.map(teamMembers, function (member) {
+                    var memberName = this.context.flux.membersStore.getMemberById(member).name;
+                    return (<input type='checkbox' value={member}>{memberName}</input>);
+                }.bind(this));
+                return options;
             },
 
             render: function () {
@@ -33,6 +47,39 @@ define(['lodash', 'React', 'constants'],
                                 <input type='text' valueLink={this.linkState('name')}></input>
                             </div>
                         </div>
+                        <div className=''>
+                            <div className=''>
+                                <span>Scrum Master:</span>
+                            </div>
+                            <div className=''>
+                                {this.teamMemberOptionsBox()}
+                            </div>
+                        </div>
+                        <div className=''>
+                            <div className=''>
+                                <span>Select Sprint Members:</span>
+                            </div>
+                            <div className=''>
+                                {this.teamMembersCheckBoxes()}
+                            </div>
+                        </div>
+                        <div className=''>
+                            <div className=''>
+                                <span>Start Date:</span>
+                            </div>
+                            <div className=''>
+                                <input type='text' valueLink={this.linkState('startDate')}></input>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <div className=''>
+                                <span>End Date:</span>
+                            </div>
+                            <div className=''>
+                                <input type='text' valueLink={this.linkState('endDate')}></input>
+                            </div>
+                        </div>
+
                     </div>
                 );
             }
