59b55437da549b68a12dfdbe16bfc9d028fd2841
Tom Enden <tome@wix.com>
2015-08-06 14:32:07 +0300

    added hover style to combo box and filtered matched members to exclude existing team members

--- src/js/components/team-management/MembersComboBox.jsx
+++ src/js/components/team-management/MembersComboBox.jsx
@@ -11,27 +11,43 @@ define(['lodash', 'React', 'constants'],
                     searchStr: ''
                 };
             },
+            invisibleMatchedMemberContainer: function () {
+                if (this.refs.matchedMemberContainer) {
+                    this.refs.matchedMemberContainer.getDOMNode().style.opacity = 0;
+                }
+            },
+            visibleMatchedMemberContainer: function () {
+                if (this.refs.matchedMemberContainer) {
+                    this.refs.matchedMemberContainer.getDOMNode().style.opacity = 1;
+                }
+            },
             changeExistingMember: function (event) {
                 var memberId = event.target.dataset.id;
                 var memberName = event.target.dataset.name;
                 this.context.flux.dispatcher.dispatchAction(constants.actionNames.CHANGE_EXISTING_MEMBER, memberId);
-                this.refs.matchedMemberContainer.getDOMNode().style.display = 'none';
+                this.invisibleMatchedMemberContainer();
                 this.refs.searchInput.getDOMNode().value = memberName;
 
             },
             getComboBox: function () {
                 var allMembers = this.context.flux.membersStore.getAllMembers();
-                return this.state.searchStr === '' ? <li className='matched-member'></li> :
+                var currentTeamMembers = this.context.flux.teamsStore.getCurrentTeam().members;
+                return this.state.searchStr === '' ?
+                    '' :
                     <div className='matched-member-container' ref='matchedMemberContainer'>
                         <ul className='matched-member-list'>
                             {
+
                                 _(allMembers).filter(function (member) {
-                                    return _.includes(member.name.toLowerCase(), this.state.searchStr.toLowerCase());
+                                    return !_.includes(currentTeamMembers, member.id) &&
+                                        _.includes(member.name.toLowerCase(), this.state.searchStr.toLowerCase());
                                 }, this)
                                     .map(function (member) {
                                         return (
-                                            <li data-id={member.id} data-name={member.name} onClick={this.changeExistingMember}
-                                                className='matched-member'>
+                                            <li data-id={member.id} data-name={member.name}
+                                                onClick={this.changeExistingMember}
+                                                className='matched-member'
+                                                key={member.id}>
                                                 {member.name}
                                             </li>
                                         );
@@ -41,6 +57,7 @@ define(['lodash', 'React', 'constants'],
                     </div>;
             },
             searchMember: function (event) {
+                this.visibleMatchedMemberContainer();
                 var searchStr = event.target.value;
                 this.setState({
                     searchStr: searchStr
@@ -50,7 +67,8 @@ define(['lodash', 'React', 'constants'],
             render: function () {
                 return (
                     <div>
-                        <input ref='searchInput' onChange={this.searchMember} className='search-input' type='text'/>
+                        <input ref='searchInput' onChange={this.searchMember}
+                               onBlur={this.invisibleMatchedMemberContainer} className='search-input' type='text'/>
                         {this.getComboBox()}
                     </div>
                 );
