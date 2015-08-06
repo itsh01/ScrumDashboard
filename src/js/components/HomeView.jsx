075a4e5950b45c538c83bf191978dcb056346fa2
Tom Enden <tome@wix.com>
2015-08-06 17:09:13 +0300

    major changes to HomeView, now contains HomeView and TeamManagement, and allows toggling between them

--- src/js/components/HomeView.jsx
+++ src/js/components/HomeView.jsx
@@ -1,60 +1,95 @@
-define(['lodash', 'React', 'components/team/TeamComponent', 'constants'], function (_, React, TeamView, constants) {
-    'use strict';
-    return React.createClass({
-        displayName: 'HomeView',
+define(['lodash', 'React', 'components/team/TeamComponent',
+        'constants', 'components/team-management/TeamManagement'],
+    function (_, React, TeamView, constants, TeamManagement) {
+        'use strict';
+        return React.createClass({
+            displayName: 'HomeView',
 
-        propTypes: {
-            currTeam: React.PropTypes.object,
-            params: React.PropTypes.object,
-            query: React.PropTypes.object
-        },
+            propTypes: {
+                currTeam: React.PropTypes.object,
+                params: React.PropTypes.object,
+                query: React.PropTypes.object
+            },
 
-        contextTypes: {
-            flux: React.PropTypes.any
-        },
+            contextTypes: {
+                flux: React.PropTypes.any
+            },
 
+            getInitialState: function () {
+                return {
+                    view: 'HomeView'
+                }
+            },
 
-        getTeams: function () {
-            return this.context.flux.teamsStore.getAllTeams();
-        },
+            getTeams: function () {
+                return this.context.flux.teamsStore.getAllTeams();
+            },
 
-        clearStorage: function () {
-            localStorage.clear();
-        },
+            clearStorage: function () {
+                localStorage.clear();
+            },
 
-        handleChangeTeam: function (e) {
-            this.context.flux.dispatcher.dispatchAction(constants.actionNames.CHANGE_CURRENT_TEAM, e.target.value);
-            var team = this.context.flux.teamsStore.getTeamById(e.target.value);
-            this.context.flux.dispatcher.dispatchAction(constants.actionNames.CHANGE_CURRENT_SPRINT, team.sprints[team.sprints.length - 1].id);
-        },
+            handleChangeTeam: function (e) {
+                this.context.flux.dispatcher.dispatchAction(constants.actionNames.CHANGE_CURRENT_TEAM, e.target.value);
+                var team = this.context.flux.teamsStore.getTeamById(e.target.value);
+                this.context.flux.dispatcher.dispatchAction(constants.actionNames.CHANGE_CURRENT_SPRINT, team.sprints[team.sprints.length - 1].id);
+            },
+            views: {
+                HomeView: 'HomeView',
+                TeamManagement: 'TeamManagement'
+            },
+            getViewComponent: function () {
+                var allTeams = this.context.flux.teamsStore.getAllTeams();
+                var currentTeam = this.context.flux.teamsStore.getCurrentTeam();
+                if (this.state.view === 'HomeView') {
+                    return (
+                        <div className="team-view-container">
+                            <TeamView currTeamId={currentTeam.id}/>
+                        </div>
+                    );
+                }
+                if (this.state.view === 'TeamManagement') {
+                    return (
+                        <div>
+                            <TeamManagement allTeams={allTeams} currentTeam={currentTeam}/>
+                        </div>
+                    );
+                }
+            },
+            changeView: function(event) {
+                var view = event.target.dataset.view;
+                this.setState({
+                    view: this.views[view]
+                });
+            },
+            render: function () {
+                var teamsOptions = _.map(this.getTeams(), function (team) {
+                    return (<option value={team.id} key={team.id}>{team.name}</option>);
+                });
+                return (
+                    <div>
+                        <div className="header">
+                            <div className="left">
+                                <span>Choose Team: </span>
+                                <select onChange={this.handleChangeTeam} value={null}>
+                                    <option value={null} disabled>-</option>
+                                    {teamsOptions}
+                                </select>
+                            </div>
 
-        render: function () {
-            var teamsOptions = _.map(this.getTeams(), function (team) {
-                return (<option value={team.id} key={team.id}>{team.name}</option>);
-            });
-            return (
-                <div>
-                    <div className="header">
-                        <div className="left">
-                            <span>Choose Team: </span>
-                            <select onChange={this.handleChangeTeam} value={null}>
-                                <option value={null} disabled>-</option>
-                                {teamsOptions}
-                            </select>
+                            <div className="right">
+                                <button className='clearButton' type='button' onClick={this.clearStorage}>Clear
+                                    LocalStorage
+                                </button>
+                                <button data-view={this.views.TeamManagement} onClick={this.changeView}>Manage Teams</button>
+                                <button data-view={this.views.HomeView} onClick={this.changeView}>Home</button>
+                            </div>
                         </div>
 
-                        <div className="right">
-                            <button className='clearButton' type='button' onClick={this.clearStorage}>Clear LocalStorage</button>
-                            <button>Manage Teams</button>
-                        </div>
+                        {this.getViewComponent()}
                     </div>
+                );
+            }
+        });
 
-                    <div className="team-view-container">
-                        <TeamView currTeamId={this.context.flux.teamsStore.getCurrentTeam().id} />
-                    </div>
-                </div>
-            );
-        }
     });
-
-});
--- src/js/components/team-management/MemberProfile.jsx
+++ src/js/components/team-management/MemberProfile.jsx
@@ -1,43 +1,44 @@
 define(['lodash', 'React', 'constants', 'DragDropMixin'],
     function (_, React, constants, DragDropMixin) {
-    'use strict';
-    return React.createClass({
-        displayName: 'Member Profile',
-        propTypes: {
-            member: React.PropTypes.object
-        },
-        contextTypes: {
-            flux: React.PropTypes.any
-        },
-        mixins: [DragDropMixin],
-        dragDrop: function () {
-            return {
-                draggable: true,
-                dropType: 'memberId',
-                dataTransfer: this.props.member.id
-            };
-        },
+        'use strict';
+        return React.createClass({
+            displayName: 'Member Profile',
+            propTypes: {
+                member: React.PropTypes.object
+            },
+            contextTypes: {
+                flux: React.PropTypes.any
+            },
+            mixins: [DragDropMixin],
+            dragDrop: function () {
+                return {
+                    draggable: true,
+                    dropType: 'memberId',
+                    dataTransfer: this.props.member.id
+                };
+            },
 
-        removeMember: function (event) {
-            var memberId = event.target.id;
-            var teamId = this.context.flux.teamsStore.getCurrentTeam().id;
-            this.context.flux.dispatcher.dispatchAction(constants.actionNames.REMOVE_MEMBER_FROM_TEAM, teamId, memberId);
-        },
-        render: function () {
-            var classSet = React.addons.classSet;
-            return (
-                <div className={classSet('member-profile', 'inline-block')}>
-                    <h3 className={classSet('member-name')}>{this.props.member.name}</h3>
-                    <img className='member-img'
-                         draggable={false}
-                         alt={this.props.member.name}
-                         src={this.props.member.image}/>
-                    <button className='remove-button' onClick={this.removeMember} id={this.props.member.id}>X</button>
-                </div>
-            );
-        }
+            removeMember: function (event) {
+                var memberId = event.target.id;
+                var teamId = this.context.flux.teamsStore.getCurrentTeam().id;
+                this.context.flux.dispatcher.dispatchAction(constants.actionNames.REMOVE_MEMBER_FROM_TEAM, teamId, memberId);
+            },
+            render: function () {
+                var classSet = React.addons.classSet;
+                return (
+                    <div className={classSet('member-profile', 'inline-block')}>
+                        <h3 className={classSet('member-name')}>{this.props.member.name}</h3>
+                        <img className='member-img'
+                             draggable={false}
+                             alt={this.props.member.name}
+                             src={this.props.member.image}/>
+                        <button className='remove-button' onClick={this.removeMember} id={this.props.member.id}>X
+                        </button>
+                    </div>
+                );
+            }
+        });
     });
-});
 
 
 
