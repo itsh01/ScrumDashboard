d31c736b3b632f6cc6bd488c53228f46d4c3c2d7
David <davidsu@wix.com>
2015-08-10 16:14:01 +0300

    working karma config

---
+++ src/tests/stores/helpers.spec.js
@@ -0,0 +1,19 @@
+define(['stores/helpers'], function (helpers) {
+    describe('helpers.js', function () {
+        it('should save to local storage', function () {
+            expect(true).toBeTruthy();
+        });
+        it('should retrieve value from local storage', function () {
+        });
+        it('should remove value from local storage', function () {
+        });
+        it('should return random guid', function () {
+        });
+        //it('should return true when given array that contains only strings', function(){});
+        //it('should return false when given array that contains something other than string', function(){});
+        it('should', function () {
+        });
+        it('should', function () {
+        });
+    });
+});
