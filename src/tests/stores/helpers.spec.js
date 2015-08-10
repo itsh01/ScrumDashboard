a902b762e566bf70b9f51da724cfd4f0d8200524
Maria Osipova <mariao@wix.com>
2015-08-10 16:59:47 +0300

    test bug fiixed

--- src/tests/stores/helpers.spec.js
+++ src/tests/stores/helpers.spec.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 define(['stores/helpers'], function (helpers) {
@@ -6,14 +5,14 @@ define(['stores/helpers'], function (helpers) {
     describe('saveToLocalStorage.js', function () {
         it('should save to local storage ', function () {
             window.localStorage.clear();
-            var mockObj = {a:1, b:2};
+            var mockObj = {a: 1, b: 2};
             var mockStoreName = 'checkstore';
             helpers.saveToLocalStorage(mockStoreName, mockObj);
             var restoredObj = helpers.restoreFromLocalStorage(mockStoreName);
 
             expect(JSON.stringify(mockObj)).toBe(JSON.stringify(restoredObj));
         });
-
+    });
 
 
     describe('generateGuid', function () {
