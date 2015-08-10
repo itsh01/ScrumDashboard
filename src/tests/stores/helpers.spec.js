2e6c90d2726676517c2563d88f60786b5d4b6ab2
Tom Enden <tome@wix.com>
2015-08-10 17:12:28 +0300

    linted

--- src/tests/stores/helpers.spec.js
+++ src/tests/stores/helpers.spec.js
@@ -6,8 +6,6 @@ define(['stores/helpers'], function (helpers) {
     describe('saveToLocalStorage.js', function () {
         it('should save to local storage ', function () {
             window.localStorage.clear();
-            var mockObj = {a: 1, b: 2};
-            var mockStoreName = 'checkstore';
             helpers.saveToLocalStorage(mockStoreName, mockObj);
             var restoredObj = helpers.restoreFromLocalStorage(mockStoreName);
 
