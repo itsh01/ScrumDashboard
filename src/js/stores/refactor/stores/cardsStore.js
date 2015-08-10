d23c2f1a87238d6998978b78e032aff64af92d3f
Maria Osipova <mariao@wix.com>
2015-08-10 13:41:58 +0300

    cards store: redundant saveToLocalStorage calls removed

--- src/js/stores/refactor/stores/cardsStore.js
+++ src/js/stores/refactor/stores/cardsStore.js
@@ -98,7 +98,6 @@ define([
                     cardWithDefaults.id = helpers.generateGuid();
                     cardWithDefaults.status = cardWithDefaults.status || 'unassigned';
                     currentCards.push(cardWithDefaults);
-                    saveToLocalStorage();
                     return cardWithDefaults.id;
                 }
             }
@@ -118,10 +117,7 @@ define([
             function updateCard(cardId, newCardData) {
                 delete newCardData.id;
                 if (isValidCard(newCardData)) {
-                    var didUpdate = helpers.updateItem(currentCards, cardId, newCardData, 'Card Store');
-                    // TODO: if assignee or team provided then make sure that they exist
-                    saveToLocalStorage();
-                    return didUpdate;
+                    return helpers.updateItem(currentCards, cardId, newCardData, 'Card Store');
                 }
                 return false;
             }
@@ -149,18 +145,6 @@ define([
             }
 
 
-            function saveToLocalStorage() {
-                helpers.saveToLocalStorage('cards', currentCards);
-            }
-
-            function restoreFromLocalStorage() {
-                return helpers.restoreFromLocalStorage('cards');
-            }
-
-            /*eslint-disable no-unused-vars */
-            function removeFromLocalStorage() {
-                helpers.removeFromLocalStorage('cards');
-            }
 
             /*eslint-enable no-unused-vars */
 
@@ -182,6 +166,22 @@ define([
                     this.emitChange();
                 }
             }.bind(this));
+
+
+
+
+            function saveToLocalStorage() {
+                helpers.saveToLocalStorage('cards', currentCards);
+            }
+
+            function restoreFromLocalStorage() {
+                return helpers.restoreFromLocalStorage('cards');
+            }
+
+            /*eslint-disable no-unused-vars */
+            function removeFromLocalStorage() {
+                helpers.removeFromLocalStorage('cards');
+            }
 
         };
 
