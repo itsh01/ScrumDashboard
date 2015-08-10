3b3d2f227af2a2300b6ec0a622c114320d2e4db2
tzabarc <tzabarc@wix.com>
2015-08-10 16:54:59 +0300

    save to storage test

--- src/tests/stores/helpers.spec.js
+++ src/tests/stores/helpers.spec.js
@@ -1,19 +1,14 @@
 define(['stores/helpers'], function (helpers) {
     describe('helpers.js', function () {
-        it('should save to local storage', function () {
-            expect(true).toBeTruthy();
+        it('should save to local storage ', function () {
+            window.localStorage.clear();
+            var mockObj = {a:1, b:2};
+            var mockStoreName = 'checkstore';
+            helpers.saveToLocalStorage(mockStoreName, mockObj);
+            var restoredObj = helpers.restoreFromLocalStorage(mockStoreName);
+
+            expect(JSON.stringify(mockObj)).toBe(JSON.stringify(restoredObj));
         });
-        it('should retrieve value from local storage', function () {
-        });
-        it('should remove value from local storage', function () {
-        });
-        it('should return random guid', function () {
-        });
-        //it('should return true when given array that contains only strings', function(){});
-        //it('should return false when given array that contains something other than string', function(){});
-        it('should', function () {
-        });
-        it('should', function () {
-        });
+
     });
 });
