d7aa1d604aa21677aa6902b92620740e2a186e0d
Nofar Cohav <nofarc@wix.com>
2015-08-11 18:12:37 +0300

    added team management tests

---
+++ src/tests/components/team-managment/TeamManagment.spec.js
@@ -0,0 +1,29 @@
+define(['React', 'components/team-management/TeamManagement', 'stubContext', 'stores/flux'],
+    function (React, TeamManagement, stubContext, Flux) {
+        'use strict';
+        console.log(stubContext);
+        //var mockTeam = {
+        //    members: ['1234']
+        //};
+        //var mockMember = {name: 'shlomo', id: '1234'};
+        var reactTestUtils = React.addons.TestUtils;
+        describe('Team management tests', function () {
+            beforeEach(function () {
+
+                //spyOn(TeamManagement.prototype.context, '').and.returnValue(mockTeam);
+                var flux = new Flux();
+                var TeamManagementWithContext = stubContext(TeamManagement, {flux: flux});
+                //spyOn(TeamManagement.prototype, 'getCurrentTeam').and.returnValue(mockTeam);
+                var instance = React.createElement(TeamManagementWithContext, {});
+                reactTestUtils.renderIntoDocument(instance);
+
+                //spyOn(teamComp.context.flux.membersStore, 'getMemberById').and.returnValue(mockMember);
+            });
+
+            it('should return member', function () {
+                //var mainComp = reactTestUtils.renderIntoDocument(React.createElement(MainContainer));
+                //var teamComp = reactTestUtils.renderIntoDocument(React.createElement(TeamManagement));
+            });
+        });
+
+    });
