1e4349b6375f841e89ecb2505281817f39460eb0
David <davidsu@wix.com>
2015-08-11 14:49:49 +0300

    planning store tests improved

--- src/tests/stores/planning.spec.js
+++ src/tests/stores/planning.spec.js
@@ -1,26 +1,20 @@
 define(['constants', 'stores/planning'], function (consts, Planning) {
     'use strict';
     describe('planning store', function () {
-        /*eslint-disable no-unused-vars*/
-        var planningStore;
-        /*eslint-enable no-unused-vars*/
-        var spyDispatcher;
-        beforeEach(function () {
 
-            spyDispatcher = jasmine.createSpyObj('spyDispatcher', ['registerAction']);
-            /*eslint-disable no-unused-vars*/
-            planningStore = new Planning(spyDispatcher);
-            /*eslint-enable no-unused-vars*/
-
-
-        });
-
-        it('spy should be up and running', function () {
-            console.log(spyDispatcher);
-            expect(spyDispatcher.registerAction).toBeDefined();
-        });
 
         describe('register actions', function () {
+            /*eslint-disable no-unused-vars*/
+            var planningStore;
+            /*eslint-enable no-unused-vars*/
+            var spyDispatcher;
+            beforeEach(function () {
+
+                spyDispatcher = jasmine.createSpyObj('spyDispatcher', ['registerAction']);
+                planningStore = new Planning(spyDispatcher);
+
+            });
+
             it('should register add card event', function () {
                 expect(spyDispatcher.registerAction).toHaveBeenCalledWith(
                     consts.actionNames.PLANNING_ADD_CARD,
@@ -42,6 +36,43 @@ define(['constants', 'stores/planning'], function (consts, Planning) {
                 );
             });
         });
+
+        describe('getters work properly', function () {
+
+
+            var planningStore;
+            var mockDispatcher;
+            beforeEach(function () {
+
+                mockDispatcher = {
+                    registerAction: function (eventName, callback) {
+                        this[eventName] = callback;
+                    }
+                };
+
+                planningStore = new Planning(mockDispatcher);
+
+            });
+
+            it('should have null as current card as default value', function () {
+                expect(planningStore.getCurrentCard()).toBeNull();
+            });
+
+            it('should return false for getIsAddingOrEditingCard', function () {
+                expect(planningStore.getIsAddingOrEditingCard()).toEqual(false);
+            });
+
+            it('should return true for getAddingOrEditingCard', function () {
+                mockDispatcher[consts.actionNames.PLANNING_ADD_CARD]();
+                expect(planningStore.getIsAddingOrEditingCard()).toEqual(true);
+            });
+
+            it('should properly set currentCard', function () {
+                mockDispatcher[consts.actionNames.PLANNING_EDIT_CARD]('val');
+                expect(planningStore.getCurrentCard()).toEqual('val');
+            });
+        });
+
 
     });
 });
