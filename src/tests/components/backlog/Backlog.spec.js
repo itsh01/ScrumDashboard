3df03cc2f215a299c1e9850303c6156bfad7b379
tzabarc <tzabarc@wix.com>
2015-08-11 14:35:41 +0300

    start of backlog tests

---
+++ src/tests/components/backlog/Backlog.spec.js
@@ -0,0 +1,20 @@
+//define(['components/backlog/Backlog'], function (backlog) {
+//    'use strict';
+//
+//    describe('backlog', function () {
+//
+//        describe('component Backlog', function () {
+//            it('should  ', function () {
+//
+//            });
+//
+//        });
+//
+//
+//
+//
+//
+//
+//    });
+//
+//});
