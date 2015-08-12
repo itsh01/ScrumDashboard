c4421b00b64431865c286079157cc86cebda31e0
tzabarc <tzabarc@wix.com>
2015-08-12 16:06:34 +0300

    backlog tests

--- src/tests/components/backlog/Backlog.spec.js
+++ src/tests/components/backlog/Backlog.spec.js
@@ -1,21 +1,43 @@
-//define([
-//        'react',
-//        'components/backlog/Backlog'
-//    ],
-//    function (React, backlog) {
-//        'use strict';
-//        describe('backlog', function () {
-//
-//            describe('component Backlog', function () {
-//                it('should  ', function () {
-//                    var instance = React.createElement(backlog, {teamId: '2d2d8f82-0b6a-404a-9c08-929cfe3de079'});
-//                    var backlogComp = React.addons.TestUtils.renderIntoDocument(instance);
-//                    expect(backlogComp.state.loaded).toBeTruthy();
-//                });
-//
-//            });
-//
-//        });
-//
-//    }
-//);
+define([
+        'react',
+        'stubContext',
+        'components/backlog/Backlog',
+        'stores/flux'
+    ],
+    function (React, stubContext, Backlog, Flux) {
+        'use strict';
+        describe('backlog', function () {
+
+
+            describe('component Backlog', function () {
+                var BacklogWithContext;
+
+                beforeEach(function () {
+                    window.localStorage.clear();
+
+                    BacklogWithContext = stubContext(Backlog, {flux: new Flux()});
+
+                });
+
+                it('should be defined', function () {
+                    var instance = React.createElement(BacklogWithContext, {teamId: '2d2d8f82-0b6a-404a-9c08-929cfe3de079'});
+                    var backlogComp = React.addons.TestUtils.renderIntoDocument(instance);
+                    expect(backlogComp).toBeDefined();
+                });
+
+                it('should have 2 cardlists components', function () {
+
+                });
+                it('should have a title', function () {
+
+                });
+                it('should  something', function () {
+
+                });
+
+            });
+
+        });
+
+    }
+);
