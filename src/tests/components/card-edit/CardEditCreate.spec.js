1aa1e2f662f6a181a1102aa0efe771e6f8bdf380
David <davidsu@wix.com>
2015-08-13 16:33:37 +0300

    adding cardEditCreate tests

--- src/tests/components/card-edit/CardEditCreate.spec.js
+++ src/tests/components/card-edit/CardEditCreate.spec.js
@@ -6,10 +6,20 @@ define([
     ],
     function (React, CardEditCreate, stubContext, Flux) {
         'use strict';
+        var instance, instanceWithContext, renderedInstance;
+
         describe('CardEditCreate', function () {
-            var flux = new Flux();
-            var instance = React.createElement(stubContext(CardEditCreate, {flux: flux}), {});
-            React.addons.TestUtils.renderIntoDocument(instance);
+            instanceWithContext = stubContext(CardEditCreate, {flux: new Flux()});
+            instance = React.createElement(instanceWithContext, {});
+
+
+            renderedInstance = React.addons.TestUtils.renderIntoDocument(instance);
+
+
+        });
+
+        it('should be creating card if there is no current card in planningStore', function () {
+            expect(React.addons.TestUtils.isCompositeComponent(renderedInstance)).toBeTruthy();
         });
     }
 );
