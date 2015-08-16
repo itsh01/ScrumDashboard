2d299ef2d528a68b62c7e73c9575122a4a612586
David <davidsu@wix.com>
2015-08-16 15:29:20 +0300

    improving card-edit tests

--- src/tests/components/card-edit/CardEditCreate.spec.js
+++ src/tests/components/card-edit/CardEditCreate.spec.js
@@ -3,28 +3,89 @@ define([
         'components/card-edit/CardEditCreate',
         'stubContext',
         'stores/flux'
-        //,'lodash'
+        , 'lodash'
     ],
-    function (React, CardEditCreate, stubContext, Flux/*, _*/) {
+    function (React, CardEditCreate, stubContext, Flux, _) {
         'use strict';
-        var comp;
+        var flux;
 
+        function initComponent() {
+            var CardEditCreateWithContext = stubContext(CardEditCreate, {flux: flux});
+            var instance = React.createElement(CardEditCreateWithContext, {});
+            var wrappedEl = React.addons.TestUtils.renderIntoDocument(instance).getWrappedElement();
+            return React.addons.TestUtils.renderIntoDocument(wrappedEl);
+        }
 
         describe('CardEditCreate', function () {
 
             beforeEach(function () {
                 localStorage.clear();
-                var flux = new Flux();
-                var CardEditCreateWithContext = stubContext(CardEditCreate, {newFlux: flux});
-                var instance = React.createElement(CardEditCreateWithContext, {});
-                var wrappedEl = React.addons.TestUtils.renderIntoDocument(instance).getWrappedElement();
-                comp = React.addons.TestUtils.renderIntoDocument(wrappedEl);
-
             });
 
             describe('empty card tests', function () {
+                var comp;
+                beforeEach(function () {
+                    flux = new Flux();
+                    comp = initComponent();
+                });
+
                 it('should be creating card if there is no current card in planningStore', function () {
                     expect(comp.isCreating()).toBe(true);
+                });
+
+                it('should have empty inputs when creating card', function () {
+                    var renderedDOM = React.findDOMNode(comp);
+                    var inputs = renderedDOM.querySelectorAll('input');
+                    _.forEach(inputs, function (input) {
+                        expect(input.textContent).toEqual('');
+                    });
+                });
+            });
+
+            describe('edit card tests', function () {
+                var mockCard, comp;
+                beforeEach(function () {
+                    mockCard = {
+                        id: 'b97fff13-de90-4e1f-abb7-39f786d11450',
+                        name: 'card 1',
+                        description: 'description',
+                        score: 1
+                    };
+                    flux = new Flux();
+                    spyOn(flux.planningStore, 'getCurrentCard').and.returnValue(mockCard);
+                    spyOn(flux.teamsStore, 'getAllActiveTeams')
+                        .and.returnValue([
+                            {id: 1, name: 'team1'},
+                            {id: 2, name: 'team2'}
+                        ]);
+                    comp = initComponent();
+
+                });
+
+                it('should be editing card if there is current card in planningStore', function () {
+                    expect(comp.isCreating()).toBe(false);
+                });
+                it('should have component state synced with given data', function () {
+
+                    expect(comp.state.name).toEqual(mockCard.name);
+                    expect(comp.state.score).toEqual(mockCard.score);
+                    expect(comp.state.description).toEqual(mockCard.description);
+                    expect(comp.state.id).toEqual(mockCard.id);
+                });
+
+                it('should have 2 inputs with value synced with given data', function () {
+                    var rend = React.addons.TestUtils.scryRenderedDOMComponentsWithTag(comp, 'input');
+                    expect(rend[0].props.valueLink.value).toEqual(mockCard.name);
+                    expect(rend[1].props.valueLink.value).toEqual(mockCard.description);
+                });
+
+                it('should set state "team" on selection change', function () {
+                    //comp.setState({team:'t'});
+                    //var rend = React.addons.TestUtils.scryRenderedDOMComponentsWithTag(comp, 'select');
+                    //var teamSelect = _.filter(rend, function (select) {
+                    //    return select.props.value === 'team'
+                    //});
+                    //expect(teamSelect.length).toEqual(1);
                 });
             });
 
