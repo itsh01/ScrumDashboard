9eca1f7d2e3607bc9215b38361cd79b8140f6454
Itay Shtekel <shtekel1@gmail.com>
2015-08-16 17:33:02 +0300

    addded travis configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
-module.exports = function(config) {
-    config.set({
+module.exports = function (config) {
+    var configuration = {
 
         // base path, that will be used to resolve files and exclude
         basePath: '',
@@ -16,12 +16,12 @@ module.exports = function(config) {
             {pattern: 'src/js/**/*.js', included: false},
 
             'test-main.js'
-            ],
+        ],
 
         // list of files to exclude
         exclude: [
             'src/vendor/require.js'
-            ],
+        ],
 
         // test results reporter to use
         // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
@@ -54,6 +54,12 @@ module.exports = function(config) {
         // - IE (only Windows)
         browsers: ['Chrome'],
 
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
 
         // If browser does not capture in given timeout [ms], kill it
         captureTimeout: 60000,
@@ -63,5 +69,11 @@ module.exports = function(config) {
         // if true, it capture browsers, run tests and exit
         singleRun: false
 
-    });
+    };
+
+    if(process.env.TRAVIS){
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
