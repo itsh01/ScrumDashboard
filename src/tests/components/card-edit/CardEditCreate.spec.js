2f3f6e76b25aad351b88c26a646fca352a52f612
David <davidsu@wix.com>
2015-08-16 11:08:08 +0300

    card-edit tests...

--- src/tests/components/card-edit/CardEditCreate.spec.js
+++ src/tests/components/card-edit/CardEditCreate.spec.js
@@ -2,14 +2,36 @@ define([
         'React',
         'components/card-edit/CardEditCreate',
         'stubContext',
-        'stores/flux'
+        'stores/flux',
+        'lodash'
     ],
-    function (React, CardEditCreate, stubContext, Flux) {
+    function (React, CardEditCreate, stubContext, Flux, _) {
         'use strict';
+        var instance, instanceWithContext, renderedInstance;
+
+
         describe('CardEditCreate', function () {
-            var flux = new Flux();
-            var instance = React.createElement(stubContext(CardEditCreate, {flux: flux}), {});
-            React.addons.TestUtils.renderIntoDocument(instance);
+            instanceWithContext = stubContext(CardEditCreate, {flux: new Flux()});
+            instance = React.createElement(instanceWithContext, {});
+            renderedInstance = React.addons.TestUtils.renderIntoDocument(instance);
+            var comp;
+
+            beforeEach(function () {
+                localStorage.clear();
+                var flux = new Flux();
+                var CardEditCreateWithContext = stubContext(CardEditCreate, {flux: flux});
+                var instance = React.createElement(CardEditCreateWithContext, {});
+                var wrappedEl = React.addons.TestUtils.renderIntoDocument(instance).getWrappedElement();
+                comp = React.addons.TestUtils.renderIntoDocument(wrappedEl);
+
+            });
+
+            describe('empty card tests', function () {
+                it('should be creating card if there is no current card in planningStore', function () {
+                    expect(comp.isCreating()).toBe(true);
+                });
+            });
+
         });
     }
 );
