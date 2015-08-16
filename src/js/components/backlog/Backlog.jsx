8e164e6a14149c415ee0a06244d1cf3a1f9b48c8
Maria Osipova <mariao@wix.com>
2015-08-16 15:36:57 +0300

    backlog store shanges listeners removed

--- src/js/components/backlog/Backlog.jsx
+++ src/js/components/backlog/Backlog.jsx
@@ -20,17 +20,7 @@ define([
                 newFlux: React.PropTypes.any
 
             },
-            componentDidMount: function () {
-                this.context.newFlux.cardsStore.addChangeListener(this.onChange);
-                this.context.newFlux.teamsStore.addChangeListener(this.onChange);
-                this.context.newFlux.membersStore.addChangeListener(this.onChange);
-            },
 
-            componentWillUnmount: function () {
-                this.context.newFlux.cardsStore.removeChangeListener(this.onChange);
-                this.context.newFlux.teamsStore.removeChangeListener(this.onChange);
-                this.context.newFlux.membersStore.removeChangeListener(this.onChange);
-            },
             onChange: function () {
                 this.setState({});
             },
