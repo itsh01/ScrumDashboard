11fd21bae9221dfe00b4a3e04e88568d43038ebf
Itay Shtekel <shtekel1@gmail.com>
2015-08-16 13:16:40 +0300

    added new flux to velocity

--- src/js/components/sprint/Velocity.jsx
+++ src/js/components/sprint/Velocity.jsx
@@ -15,13 +15,27 @@ define([
                 sprintMembers: React.PropTypes.array
             },
             contextTypes: {
-                flux: React.PropTypes.any
+                flux: React.PropTypes.any,
+                newFlux: React.PropTypes.any
             },
             mixins: [HistoryMixin],
             getDefaultProps: function () {
                 return {
                     cardLifecycle: ['Backlog', 'In progress', 'Done']
                 };
+            },
+            componentDidMount: function () {
+                this.context.newFlux.cardsStore.addChangeListener(this.onChange);
+                this.context.newFlux.teamsStore.addChangeListener(this.onChange);
+                this.context.newFlux.membersStore.addChangeListener(this.onChange);
+            },
+            componentWillUnmount: function () {
+                this.context.newFlux.cardsStore.removeChangeListener(this.onChange);
+                this.context.newFlux.teamsStore.removeChangeListener(this.onChange);
+                this.context.newFlux.membersStore.removeChangeListener(this.onChange);
+            },
+            onChange: function () {
+                this.forceUpdate();
             },
             getSprintCards: function () {
                 var cards = [],
@@ -31,7 +45,7 @@ define([
                     cards = _.map(retro, this.mapHistoryToCards);
                 } else {
                     _.forEach(this.props.sprintMembers, function (memberId) {
-                        cards = cards.concat(this.context.flux.cardsStore.getUserCards(memberId));
+                        cards = cards.concat(this.context.newFlux.cardsStore.getUserCards(memberId));
                     }, this);
                 }
                 return cards;
