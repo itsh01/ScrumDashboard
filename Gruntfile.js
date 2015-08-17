3c4e0d23c9b84a48d7db1c0eca997091b56c22ee
Itay Shtekel <shtekel1@gmail.com>
2015-08-17 13:43:54 +0300

    splited grunt to build and dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,17 +6,23 @@
 
 module.exports = function (grunt) {
 
-    var VENDOR_TARGET = 'src/vendor/';
-    var VENDOR_STYLE_TARGET = 'src/stylesheets/vendor/';
+    var VENDOR_TARGET = 'build/vendor/';
+    var VENDOR_STYLE_TARGET = 'build/css/vendor/';
 
     grunt.initConfig({
         clean: {
             main: {
                 src: [VENDOR_TARGET + '**/*']
+            },
+            build: {
+                src: 'build/**/*'
+            },
+            dist: {
+                src: 'dist/**/*'
             }
         },
         copy: {
-            main: {
+            build: {
                 files: [
                     {
                         src: 'node_modules/requirejs/require.js',
@@ -61,20 +67,40 @@ module.exports = function (grunt) {
                     {
                         src: 'node_modules/eventemitter2/lib/eventemitter2.js',
                         dest: VENDOR_TARGET + 'eventemitter2.js'
-                    }
-                ]
-            },
-            build: {
-                files: [
+                    },
                     {
-                        src: VENDOR_TARGET + 'require.js',
-                        dest: 'build/vendor/require.js'
+                        src: 'node_modules/requirejs/require.js',
+                        dest: VENDOR_TARGET + 'require.js'
                     },
                     {
                         expand: true,
                         cwd: 'src/img/',
                         src: '**/*',
                         dest: 'build/img'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/js',
+                        src: '**/*.js',
+                        dest: 'build/js'
+                    },
+                    {
+                        src: 'src/index.html',
+                        dest: 'build/index.html'
+                    }
+                ]
+            },
+            dist: {
+                files: [
+                    {
+                        src: 'build/vendor/require.js',
+                        dest: 'dist/vendor/require.js'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'build/img/',
+                        src: '**/*',
+                        dest: 'dist/img'
                     }
                 ]
             }
@@ -104,7 +130,7 @@ module.exports = function (grunt) {
                         expand: true,
                         cwd: 'src/js/',
                         src: ['**/*.jsx'],
-                        dest: 'src/js/',
+                        dest: 'build/js/',
                         ext: '.js'
                     }
                 ]
@@ -119,14 +145,7 @@ module.exports = function (grunt) {
                 }
             },
             dist: {
-                text: 'Distributing...',
-                options: {
-                    font: 'doom',
-                    log: true
-                }
-            },
-            danger: {
-                text: 'Danger  Build !!!',
+                text: 'Publishing...',
                 options: {
                     font: 'doom',
                     log: true
@@ -143,20 +162,7 @@ module.exports = function (grunt) {
                     '!src/js/*.js',
                     'Gruntfile.js'
                 ],
-                tasks: ['dev'],
-                options: {
-                    debounceDelay: 500
-                }
-            },
-            react: {
-                files: [
-                    'src/**/*.jsx'
-                ],
-                tasks: [
-                    'asciify:danger',
-                    'log:"\nDO NOT COMMIT BEFORE BUILDING!!\n"',
-                    'babel'
-                ],
+                tasks: ['default'],
                 options: {
                     debounceDelay: 500
                 }
@@ -165,9 +171,9 @@ module.exports = function (grunt) {
         requirejs: {
             compile: {
                 options: {
-                    baseUrl: 'src/js',
-                    mainConfigFile: 'src/js/main.js',
-                    out: 'build/js/main.min.js',
+                    baseUrl: 'build/js',
+                    mainConfigFile: 'build/js/main.js',
+                    out: 'dist/js/main.min.js',
                     name: 'main',
                     optimization: 'uglify',
                     preserveLicenseComments: false
@@ -177,7 +183,7 @@ module.exports = function (grunt) {
         processhtml: {
             build: {
                 files: {
-                    'build/index.html': ['src/index.html']
+                    'dist/index.html': ['build/index.html']
                 }
             }
         },
@@ -186,8 +192,8 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     src: 'main.css',
-                    dest: 'build/css',
-                    cwd: 'src/stylesheets',
+                    dest: 'dist/css',
+                    cwd: 'build/css',
                     ext: '.min.css'
                 }]
             }
@@ -222,7 +228,7 @@ module.exports = function (grunt) {
                     expand: true,
                     cwd: 'src/stylesheets',
                     src: '**/*.scss',
-                    dest: 'src/stylesheets',
+                    dest: 'build/css',
                     ext: '.css'
                 }]
             }
@@ -247,10 +253,15 @@ module.exports = function (grunt) {
 
     grunt.registerTask('lint', ['eslint', 'scsslint']);
     grunt.registerTask('compile', ['sass', 'umd', 'babel']);
-    grunt.registerTask('dev', ['asciify:build', 'lint', 'compile', 'test']);
     grunt.registerTask('minify', ['processhtml', 'requirejs', 'cssmin']);
-    grunt.registerTask('build', ['asciify:build', 'lint', 'clean', 'compile', 'copy', 'minify']);
     grunt.registerTask('test', ['karma']);
+
+    grunt.registerTask('build-process', ['lint', 'clean:build', 'compile', 'copy:build']);
+    grunt.registerTask('build', ['asciify:build', 'build-process']);
+
+    grunt.registerTask('publish-process', ['clean:dist', 'minify', 'copy:dist']);
+    grunt.registerTask('publish', ['asciify:dist', 'build-process', 'publish-process']);
+
     grunt.registerTask('default', ['build', 'test']);
 
 };
